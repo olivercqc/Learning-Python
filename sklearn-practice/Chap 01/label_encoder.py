2cdb4be518cbdac8df7247e904c0864dc9e33a26
olivercqc <olivercqc@foxmail.com>
2018-04-08 16:26:17 +0800

    2018-04-08 update

---
+++ sklearn-practice/Chap 01/label_encoder.py
@@ -0,0 +1,15 @@
+from sklearn import preprocessing
+label_encoder = preprocessing.LabelEncoder()
+input_classes = ['audi', 'ford', 'audi', 'toyota', 'ford', 'bmw']
+label_encoder.fit(input_classes)
+print("Classes mapping:")
+for i, item in enumerate(label_encoder.classes_):
+    print(item, "-->", i)
+labels = ['toyota', 'ford', 'audi']
+encoded_labels = label_encoder.transform(labels)
+print("Labels =", labels)
+print("Encoded labels =", list(encoded_labels))
+encoded_labels = [2, 1, 0, 3, 1]
+decoded_labels = label_encoder.inverse_transform(encoded_labels)
+print("Encoded labels =", encoded_labels)
+print("Decoded labels =", list(decoded_labels))
